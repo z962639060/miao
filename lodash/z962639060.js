8c42ccbe7bd808df66e929f6b7c04fe5f21dda22
zzx <zzx@qq.com>
2023-06-05 09:59:11 +0800

    1

---
+++ lodash/z962639060.js
@@ -0,0 +1,25 @@
+var z962639060 = {
+  chunk: function (arr,num){
+    var result = [];
+    for(var i = 0,len = arr.length;i < len;i = i+num){
+      result.push(arr.slice(i,i+num));
+    }
+    return result
+  },
+  
+  compact: function (ary){
+    let result = [];
+    arr.map(item =>{
+      if(item)
+        result.push(item)
+    })
+    return result
+  },
+
+  difference: function (arr,del){
+    return arr.filter((value)=>{
+      return del.index0f(value) < 0;
+    });
+  }
+
+}
